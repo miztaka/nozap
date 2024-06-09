f9d538f36fdead81e4de94bb73a774b7a088f15f
Mitsutaka Sato <miztaka@honestyworks.jp>
2024-06-09 14:33:08 +1200

    Polish layout

--- src/components/VideoCard.vue
+++ src/components/VideoCard.vue
@@ -8,8 +8,14 @@
       width="200"
       cover
     ></v-img>
-    <v-card-title v-tooltip:bottom="item.snippet.title">{{ item.snippet.title }}</v-card-title>
+    <v-card-text>
+      <div class="text-subtitle-1 text-left">{{ item.snippet.title }}</div>
+      <div class="font-weight-light text-right">{{ formatDate(item.snippet.publishedAt) }}</div>
+    </v-card-text>
     <v-card-subtitle>{{ item.snippet.channelTitle }}</v-card-subtitle>
+    <!--
+    <v-card-title v-tooltip:bottom="item.snippet.title">{{ item.snippet.title }}</v-card-title>
+    -->
   </v-card>
 </template>
 <script>
@@ -20,6 +26,9 @@ export default {
       const th = item.snippet.thumbnails
       const obj = th.medium || th.standard || th.default
       return obj ? obj.url : ''
+    },
+    formatDate(datestr) {
+      return datestr.split('T')[0]
     }
   }
 }
